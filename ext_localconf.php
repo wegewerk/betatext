e0f58904d47c0110039ee1c7594848bb9b477836
Moritz Karbaum <mkb@wegewerk.com>
2017-02-21 12:55:20 +0100

    [fix] disabled betatext still shows comments

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,10 +10,10 @@ if (TYPO3_MODE=='FE'){
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['sr_feuser_register']['extendingTCA'][] = $_EXTKEY;
 }
 
-if (!function_exists('user_betatext_enabled')) {
-	function user_betatext_enabled()
+if (!function_exists('user_betatext_doktypesmatch')) {
+	function user_betatext_doktypesmatch()
 	{
 		$extSettings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['we_betatext']);
-		return $extSettings['dokType'] == $GLOBALS['TSFE']->page['doktype'] && $GLOBALS['TSFE']->page['tx_webetatext_enable'] == 1;
+		return $extSettings['dokType'] == $GLOBALS['TSFE']->page['doktype'];
 	}
 }
