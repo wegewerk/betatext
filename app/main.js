34b6be2a0dac1b4d72772ca98c0c808463d44b7f
Moritz Karbaum <mkb@wegewerk.com>
2017-03-06 19:03:01 +0100

    [task] concat javascripts per nodejs task; fixed document not being ready.

--- app/main.js
+++ app/main.js
@@ -1,33 +1,34 @@
-requirejs.onError = function (err) {}
 
-require.config({
-		baseUrl: 'typo3conf/ext/we_betatext/app',
-		paths: {
-			text: '../lib/requirejs/text'
-		}
-});
+$(function() {
+	requirejs.onError = function (err) {}
 
-// wenn Tool aktiviert, dann auch noch Klasse im BODY
-if( bbt_enabled )
-{
-	$('body').addClass('bbt_enabled')
-	// UND body unselectierbar machen:
-	.addClass('unselectable')
-	// auch für den IE:
-	.attr('unselectable', 'on');
+	require.config({
+			baseUrl: 'typo3conf/ext/we_betatext/app',
+			paths: {
+				text: '../lib/requirejs/text'
+			}
+	});
 
-}
+	// wenn Tool aktiviert, dann auch noch Klasse im BODY
+	if( bbt_enabled )
+	{
+		$('body').addClass('bbt_enabled')
+		// UND body unselectierbar machen:
+		.addClass('unselectable')
+		// auch für den IE:
+		.attr('unselectable', 'on');
 
-// Start the main app logic.
-require(['views/appView'],
-	function (AppView) {
-
-		$(bbt_selector).wrapInner('<div class="commentable_text"></div>');
-		rangy.init();
-		var App = new AppView({
-					commentableText: $(".commentable_text").get(0)
-			});
 	}
-);
 
+	// Start the main app logic.
+	require(['views/appView'],
+		function (AppView) {
 
+			$(bbt_selector).wrapInner('<div class="commentable_text"></div>');
+			rangy.init();
+			var App = new AppView({
+						commentableText: $(".commentable_text").get(0)
+				});
+		}
+	);
+});
---
+++ lib/build/readme.txt
@@ -0,0 +1,5 @@
+Follow these steps if you changed the Javascript files and wish to build new minified versions:
+
+1. install node and npm
+2. run "npm i"
+3. run "node build.js"
